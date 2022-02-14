9b3d3de2a489561f0b78b4699d2077e987be16ba
Emiliano Cassoni <cassoniemiliano@gmail.com>
2022-02-14 11:11:53 -0300

    finalizacion styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
-import React from 'react'
 
 function Header() {
   return (
-    <ul className='flex ml-auto w-full font-bold'>
-        <li className='text-xs text-gray-800 ml auto mr-6 border-green-400 cursor-pointer'>Weather</li>
-        <li className='text-xs text-gray-800 mr-6 alert-notice cursos-pointer border-b-2 hover:border-green-400'>Alerts</li>
-        <li className='text-xs text-gray-800 mr-6 cursor-pointer border-b-2 hover:border-green-400'>Map</li>
-        <li className='text-xs text-gray-800 mr-6 cursor-pointer border-b-2 hover:border-green-400'>Satelite</li>
-        <li className='text-xs text-gray-800 ml border-b-2 cursor-pointer hover:border-green-400'>News</li>
-    </ul>
+      <ul className="flex ml-auto w-full font-bold">
+          <li className="text-xs text-gray-800 ml-auto mr-6 border-b-2 border-green-400 cursor-pointer">Weather</li>
+          <li className="text-xs text-gray-800 mr-6 alert-notice cursor-pointer border-b-2 hover:border-green-400">Alerts</li>
+          <li className="text-xs text-gray-800 mr-6 cursor-pointer border-b-2 hover:border-green-400">Map</li>
+          <li className="text-xs text-gray-800 mr-6 cursor-pointer border-b-2 hover:border-green-400">Satellite</li>
+          <li className="text-xs text-gray-800 cursor-pointer border-b-2 hover:border-green-400">News</li>
+      </ul>
   )
 }
 
